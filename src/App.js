f73d069ad544ba699001835ee66fb682dd0e1939
unknown <gopeshwarkumark@gmail.com>
2025-01-30 14:09:17 +0530

    first comit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import PageRoutes from './AllPageRoutes/PageRoutes'
-import Quest from './Components/Quest'
 
 // pdjx bzqm pdij whae
 function App() {
-  // localStorage.clear()
    return (
     <>
-    {/* <PageRoutes/> */}
-    <Quest/>
+    <PageRoutes/>
     </>
   )
 }
